2cdf13d868abd52b10facf9c9539ae6cda998081
Greg K <gkreisman_cep@qq.com>
2026-01-13 16:23:05 +0800

    make video smaller for beter mobile

--- day13/sketch.js
+++ day13/sketch.js
@@ -14,14 +14,15 @@ function preload() {
 
 function setup() {
   cnv = createCanvas(640, 480);
+  pixelDensity(1);
   let cx = (windowWidth - cnv.width) / 2;
   let cy = (windowHeight - cnv.height) / 2;
   cnv.position(cx, cy);
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(160, 120); // video size is small but same ratio
   video.hide();
   bodySegmentation.detectStart(video, gotResults);
-  pixelDensity(1);
+
   longText = rawText.repeat(150);
 
   textFont("monospace");
@@ -33,20 +34,17 @@ function draw() {
   background(255);
 
   if (segmentation && segmentation.mask) {
-    // 1. Flip ONLY the mask image
+    // flip so it is mirrored
     push();
     translate(width, 0);
     scale(-1, 1);
+    // draw small segmentation mask to whole canvas, same ratio
     image(segmentation.mask, 0, 0, width, height);
     pop();
-    // Now the coordinate system is back to normal!
-
-    // 2. Draw the text normally (readable from left to right)
+    // text
     fill(0);
     noStroke();
-
-    // The black text will still "disappear" into the black background
-    // of the flipped mask, but the letters stay facing the right way.
+    // draw text box over entire canvas in parts over black will not show
     text(longText, 0, 0, width, height);
   }
 }
